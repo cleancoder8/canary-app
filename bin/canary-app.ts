643d278ffb3880995b9ab511237e063d4d8e1f90
Ayush Dhar <ayushdhar@ayushs-air.lan>
2021-11-13 18:45:50 -0500

    Initial commit

---
+++ bin/canary-app.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { CanaryAppStack } from '../lib/canary-app-stack';
+
+const app = new cdk.App();
+new CanaryAppStack(app, 'CanaryAppStack');
