f47eb0acacff45e068fe38f7c6944ca40447ec1b
Ayush Dhar <ayushdhar@ayushs-air.lan>
2021-12-05 14:18:29 -0500

    initial2

--- lib/canary-app-stack.ts
+++ lib/canary-app-stack.ts
@@ -1,18 +1,16 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as ssm from '@aws-cdk/aws-ssm'
 import * as cdk from '@aws-cdk/core';
 
 export class CanaryAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CanaryAppQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CanaryAppTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+        new ssm.StringParameter(this, "ssm-parameter", {
+            allowedPattern: '.*',
+            description: 'The value Foo',
+            parameterName: 'FooParameter',
+            stringValue: 'Foo',
+            tier: ssm.ParameterTier.ADVANCED,
+        })
+    }
 }
